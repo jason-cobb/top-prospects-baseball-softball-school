caed2976604fe462122f555b67c947052172c912
Jason Cobb <jasoncobbatc@gmail.com>
2021-11-07 21:31:07 -0500

    hamburger icon add plus attempt to make responsive

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-app.js
+
+//const button = document.getElementById('hamburger')
+
+//button.addEventListener('click', function(){
+    
+    
+   // const hamburger = document.getElementById('X');
+
+  //  if (X.classList.contains('d-none')){
+   //     X.classList.remove('d-none');
+    }
+   // else{
+    //    X.classList.add('d-none');
+
+  //  }
+//});
+
